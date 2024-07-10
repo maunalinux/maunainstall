c8991a8aa5a9e1ebf145c0ce2c9744139aaf73b9
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2024-07-10 18:57:49 -0300

    Don't try to fix description formatting.

--- usr/lib/mauna/maunainstall/maunainstall.py
+++ usr/lib/mauna/maunainstall/maunainstall.py
@@ -3038,8 +3038,7 @@ class Application(Gtk.Application):
         app_description = self.builder.get_object("application_description")
 
         if description not in (None, ''):
-            subbed = re.sub(r'\n+', '\n\n', description).rstrip()
-            app_description.set_label(subbed)
+            app_description.set_label(description)
             app_description.show()
         else:
             app_description.hide()
