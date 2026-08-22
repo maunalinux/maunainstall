3cb879c999487da29932fead82c6c62598812e9a
Mauna Linux <78545394+maunalinux@users.noreply.github.com>
2026-08-21 21:33:34 -0300

    reviews.py: Fix python 3.14 multiprocessing error.

--- usr/lib/mauna/maunainstall/reviews.py
+++ usr/lib/mauna/maunainstall/reviews.py
@@ -148,22 +148,6 @@ class ReviewCache(GObject.Object):
 
         return cache, size
 
-    def _save_cache(self, cache, size):
-        path = None
-
-        try:
-            path = Path(REVIEWS_CACHE)
-            path.parent.mkdir(parents=True, exist_ok=True)
-        except Exception:
-            path = None
-        finally:
-            try:
-                with path.open(mode='w', encoding="utf8") as f:
-                    pobj = JsonObject(cache, size)
-                    json.dump(pobj, f, default=lambda o: o.__dict__, indent=4)
-            except Exception as e:
-                print("MaunaInstall: Could not save review cache: %s" % str(e))
-
     def _update_cache(self):
         thread = threading.Thread(target=self._update_reviews_thread)
         thread.start()
@@ -177,7 +161,7 @@ class ReviewCache(GObject.Object):
         success = multiprocessing.Value('b', False)
 
         current_size = multiprocessing.Value('d', self._size)
-        self.proc = multiprocessing.Process(target=self._update_cache_process, args=(success, current_size))
+        self.proc = multiprocessing.Process(target=_update_cache_process, args=(success, current_size))
         self.proc.start()
 
         self.proc.join()
@@ -192,52 +176,68 @@ class ReviewCache(GObject.Object):
     def emit_reviews_updated(self, data=None):
         self.emit("reviews-updated")
 
-    def _update_cache_process(self, success, current_size):
-        new_reviews = {}
+def _save_cache(cache, size):
+    path = None
 
+    try:
+        path = Path(REVIEWS_CACHE)
+        path.parent.mkdir(parents=True, exist_ok=True)
+    except Exception:
+        path = None
+    finally:
         try:
-            r = requests.head("https://community.linuxmint.com/data/new-reviews.list", timeout=10)
-
-            if r.status_code == 200:
-                if int(r.headers.get("content-length")) != current_size.value:
-
-                    r = requests.get("https://community.linuxmint.com/data/new-reviews.list", timeout=30)
-
-                    last_package = None
-
-                    for line in r.iter_lines():
-                        decoded = line.decode()
-
-                        elements = decoded.split("~~~")
-                        if len(elements) == 5:
-                            review = Review(elements[0], float(elements[1]), elements[2], elements[3], elements[4])
-                            if last_package is not None and last_package.name == elements[0]:
-                                #Comment is on the same package as previous comment.. no need to search for the package
-                                last_package.reviews.append(review)
-                            else:
-                                if last_package is not None:
-                                    last_package.update_stats()
-
-                                try:
-                                    package = new_reviews[elements[0]]
-                                except Exception:
-                                    package = ReviewInfo(elements[0])
-                                    new_reviews[elements[0]] = package
-
-                                last_package = package
-                                package.reviews.append(review)
-
-                    if last_package is not None:
-                        last_package.update_stats()
-
-                    self._save_cache(new_reviews, r.headers.get("content-length"))
-                    print("MaunaInstall: Downloaded new reviews")
-                    success.value = True
-                else:
-                    print("MaunaInstall: No new reviews")
-            else:
-                print("MaunaInstall: Could not download updated reviews: %s" % r.reason)
-                success.value = False
+            with path.open(mode='w', encoding="utf8") as f:
+                pobj = JsonObject(cache, size)
+                json.dump(pobj, f, default=lambda o: o.__dict__, indent=4)
         except Exception as e:
-            print("MaunaInstall: Problem attempting to access reviews url: %s" % str(e))
+            print("MaunaInstall: Could not save review cache: %s" % str(e))
+
+def _update_cache_process(success, current_size):
+    new_reviews = {}
+
+    try:
+        r = requests.head("https://community.linuxmint.com/data/new-reviews.list", timeout=10)
+
+        if r.status_code == 200:
+            if int(r.headers.get("content-length")) != current_size.value:
+
+                r = requests.get("https://community.linuxmint.com/data/new-reviews.list", timeout=30)
+
+                last_package = None
+
+                for line in r.iter_lines():
+                    decoded = line.decode()
+
+                    elements = decoded.split("~~~")
+                    if len(elements) == 5:
+                        review = Review(elements[0], float(elements[1]), elements[2], elements[3], elements[4])
+                        if last_package is not None and last_package.name == elements[0]:
+                            #Comment is on the same package as previous comment.. no need to search for the package
+                            last_package.reviews.append(review)
+                        else:
+                            if last_package is not None:
+                                last_package.update_stats()
+
+                            try:
+                                package = new_reviews[elements[0]]
+                            except Exception:
+                                package = ReviewInfo(elements[0])
+                                new_reviews[elements[0]] = package
+
+                            last_package = package
+                            package.reviews.append(review)
+
+                if last_package is not None:
+                    last_package.update_stats()
+
+                _save_cache(new_reviews, r.headers.get("content-length"))
+                print("MaunaInstall: Downloaded new reviews")
+                success.value = True
+            else:
+                print("MintInstall: No new reviews")
+        else:
+            print("MaunaInstall: Could not download updated reviews: %s" % r.reason)
+            success.value = False
+    except Exception as e:
+        print("MaunaInstall: Problem attempting to access reviews url: %s" % str(e))
 
